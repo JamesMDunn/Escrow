462372b3666bae578e28fe8e7e202cc9e955d3ae
James Dunn <jamesallstar21@gmail.com>
2021-08-17 00:26:36 -0400

    switch externals to public. able to pay original depositer

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -7,8 +7,8 @@ contract Escrow {
     event Registered(uint256 _id, address _depositer, address _withdrawer);
 
     struct Agreement {
-        address payable depositor;
-        address payable withdrawer;
+        address depositor;
+        address withdrawer;
         uint256 value;
         uint256 payBlockInterval;
         uint256 payRate;
@@ -25,15 +25,15 @@ contract Escrow {
         console.log("Deploying a Escrow Smart Contract!");
     }
 
-    function register(address payable _to, uint256 _payBlockInterval)
-        external
+    function register(address _to, uint256 _payBlockInterval)
+        public
         payable
         returns (bool)
     {
         require(_to != msg.sender, "Cannot register the same address");
         require(_to != address(this), "Cannot register this contracts address");
         escrowId++;
-        address payable _depositer = payable(msg.sender);
+        address _depositer = msg.sender;
         uint256 _amount = msg.value;
         uint256 _payRate = _amount / _payBlockInterval;
         uint256 _lastActivityBlock = block.number;
@@ -55,7 +55,7 @@ contract Escrow {
         return true;
     }
 
-    function withdraw(uint256 _id) external {
+    function withdraw(uint256 _id) public {
         require(
             agreements[_id].withdrawer == msg.sender,
             "you are not the withdrawer"
@@ -76,11 +76,11 @@ contract Escrow {
             return;
         }
         _agreement.value -= _agreement.payRate;
-        _agreement.withdrawer.call{value: _agreement.payRate};
+        payable(msg.sender).transfer(_agreement.payRate);
         _agreement.paidOut += _agreement.payRate;
     }
 
-    function depositorWithdraw(uint256 _id) external {
+    function depositorWithdraw(uint256 _id) public {
         require(
             agreements[_id].depositor == msg.sender,
             "your are not the depositer of this agreement"
@@ -90,20 +90,20 @@ contract Escrow {
             "this agreement has expired"
         );
         Agreement storage _agreement = agreements[_id];
-
-        _agreement.depositor.call{value: _agreement.value};
+        console.log("this is sender", msg.sender);
+        payable(msg.sender).transfer(_agreement.value);
         _agreement.value = 0;
         _agreement.isExpired = true;
     }
 
-    function increaseAgreementPay(uint256 _id) external payable {
+    function increaseAgreementPay(uint256 _id) public payable {
         require(agreements[_id].depositor == msg.sender);
         require(agreements[_id].isExpired == false);
         Agreement storage _agreement = agreements[_id];
         _agreement.value += msg.value;
     }
 
-    function extendAgreement(uint256 _id, uint256 _num) external {
+    function extendAgreement(uint256 _id, uint256 _num) public {
         require(agreements[_id].depositor == msg.sender);
         require(agreements[_id].isExpired == false);
         Agreement storage _agreement = agreements[_id];
